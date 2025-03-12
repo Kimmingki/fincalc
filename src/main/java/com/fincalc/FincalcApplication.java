a1a9d351350159a261a0147a21148463736629cf
Kimmingki <a01050919568@gmail.com>
2025-03-12 11:22:32 +0900

    First Commit

---
+++ src/main/java/com/fincalc/FincalcApplication.java
@@ -0,0 +1,13 @@
+package com.fincalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fincalc/FincalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
